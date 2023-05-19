6e1cc5d0ec1d5dfac197de30300b0da3fb708451
Ganesh pawar <91321109+maggipawar@users.noreply.github.com>
2023-05-19 13:16:31 +0530

    English dictionary project done

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-const btnEl = document.getElementById("btn");
-const jokeEl = document.getElementById("joke");
-const apiKey = "/EW3RRLvJKDFPyXvKeWuXQ==j6ZxcAdWBHHdHDN5";
-const apiURl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+const inputEl = document.getElementById("input");
+const infoTextEl = document.getElementById("info-text");
+const meaningContainerEl = document.getElementById("meaning-container");
+const titleEl = document.getElementById("title");
+const meaningEl = document.getElementById("meaning");
+const audioEl = document.getElementById("audio");
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-API-KEY": apiKey,
-  },
-};
-
-async function getJoke() {
+async function fetchAPI(word) {
   try {
-    jokeEl.innerHTML = "Updating...";
-    btnEl.disabled = true;
-    btnEl.innerHTML = "Loading...";
-    const response = await fetch(apiURl, options);
-    const data = await response.json();
-    btnEl.disabled = false;
-    btnEl.innerHTML = "Tell Me a Joke";
-    // console.log(data[0].joke);
-    jokeEl.innerHTML = data[0].joke;
+    infoTextEl.style.display = "block";
+    meaningContainerEl.style.display = "none";
+    infoTextEl.innerText = `Searching the meaning of "${word}"`;
+    const apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const result = await fetch(apiURL);
+    const response = await result.json();
+
+    if (response.title) {
+      meaningContainerEl.style.display = "block";
+      infoTextEl.style.display = "none";
+      titleEl.innerText = word;
+      meaningEl.innerText = "N/A";
+      audioEl.style.display = "none";
+    } else {
+      infoTextEl.style.display = "none";
+      meaningContainerEl.style.display = "block";
+      titleEl.innerText = response[0].word;
+      meaningEl.innerText = response[0].meanings[0].definitions[0].definition;
+      audioEl.src = response[0].phonetics[0].audio;
+    }
   } catch (error) {
-    jokeEl.innerHTML = "An error happened,try again later";
-    btnEl.disabled = false;
-    btnEl.innerHTML = "Tell Me a Joke";
     console.log(error);
+    infoTextEl.innerText = `an error happened, try again later`;
   }
 }
 
-btnEl.addEventListener("click", getJoke);
+inputEl.addEventListener("keyup", (e) => {
+  if (e.target.value && e.key === "Enter") {
+    fetchAPI(e.target.value);
+  }
+});
